d29dc7a5a5245e0977e32694506cf9692971df57
ivanrifki <ivanrifkinuralif@gmail.com>
2025-05-18 10:39:19 +0700

    testing ubah title link web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -65,7 +65,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "ab9958c316885610f8783faa4dfaa5e1
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "da530345592e17646d8f9fb73a33b096",
+"flutter_bootstrap.js": "92b6e66192a34c4e97ed07efe6c5122b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
